594e88f31dd285eb62106ddcac1b6ce2e6985cf1
axel92b <axel92b@gmail.com>
2020-09-25 18:56:50 +0300

    added code

---
+++ ProcKillerService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcKillerService
+{
+    class ProcKillerService
+    {
+        private Task _monitorTask;
+        private CancellationTokenSource _cts;
+        private readonly string _processName;
+
+        public ProcKillerService(string processName)
+        {
+            _processName = processName;
+        }
+
+        public bool Start()
+        {
+            try
+            {
+                _cts = new CancellationTokenSource();
+                _monitorTask = ObserverTask(_cts.Token);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public bool Stop()
+        {
+            _cts.Cancel();
+
+            try
+            {
+                _monitorTask.GetAwaiter().GetResult();
+            }
+            catch (Exception)
+            {
+                //OK
+            }
+
+            _cts.Dispose();
+            return true;
+        }
+
+        private async Task ObserverTask(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Process.GetProcesses().Where(pr => pr.ProcessName.Equals(_processName, StringComparison.OrdinalIgnoreCase)).AsParallel()
+                    .ForAll(x => x.Kill());
+                await Task.Delay(1000, token);
+            }
+            token.ThrowIfCancellationRequested();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.Win32;
+using Topshelf;
+
+namespace ProcKillerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = string.Empty;
+
+            HostFactory.Run(x =>
+            {
+                x.AddCommandLineDefinition("name", p =>
+                {
+                    name = p;
+                });
+                x.ApplyCommandLine();
+                x.AfterInstall(ihc =>
+                {
+                    //save process name in registry
+                    using var system = Registry.LocalMachine.OpenSubKey("System");
+                    using var currentControlSet = system.OpenSubKey("CurrentControlSet");
+                    using var services = currentControlSet.OpenSubKey("Services");
+                    using var service = services.OpenSubKey(ihc.ServiceName, true);
+                    const string v = "ImagePath";
+                    var imagePath = (string)service.GetValue(v);
+                    service.SetValue(v, imagePath + $" -name:\"{name}\" ");
+                });
+                x.Service<ProcKillerService>(s =>
+                {
+                    s.ConstructUsing(v => new ProcKillerService(name));
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.EnableServiceRecovery(r => r.RestartService(TimeSpan.FromSeconds(5)));
+                x.SetServiceName("Process Killer Service");
+                x.SetDescription($"Process Killer Service - Monitors and kills specified process. Configured to kill:\"{name}\".");
+                x.StartAutomatically();
+            });
+        }
+    }
+}
